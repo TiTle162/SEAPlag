ad591d07b7064ee337db507daf1f76ff75f99c7c
Sirawit Thitisoontornlak <62160162@go.buu.ac.th>
2022-12-24 15:49:48 +0700

    Change datasets for graph output.

---
+++ seaplag/src/assets/datasets/submissions/62160008/Bird.java
@@ -0,0 +1,16 @@
+public class Bird {
+    /*
+    Instance variables: states of Cat
+     */
+    String name;
+    int age;
+    String color;
+    String breed;
+
+    /*
+    Instance methods: behaviors of Cat
+     */
+    void sleep(){
+        System.out.println("Sleeping");
+    }
+}
